1590540c5484143e05a5058d606be97334fb6ab8
Milan Hauth <milahu@gmail.com>
2022-12-03 19:27:43 +0100

    update demo

---
+++ demo/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite"
+import path from 'path'
+
+const assetsDir = '';
+const outputDefaults = {
+  // remove hashes from filenames
+  entryFileNames: `${assetsDir}[name].js`,
+  chunkFileNames: `${assetsDir}[name].js`,
+  assetFileNames: `${assetsDir}[name].[ext]`,
+}
+
+export default defineConfig({
+  base: "./",
+  clearScreen: false,
+  build: {
+    outDir: path.resolve('../docs'),
+    emptyOutDir: true,
+    target: 'esnext',
+    minify: false,
+    rollupOptions: {
+      output: {
+        ...outputDefaults,
+      }
+    },
+  },
+  /*
+  assetsInclude: function assetsInclude(file) {
+    return /\.(txt)$/.test(file)
+  },
+  */
+})
